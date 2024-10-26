04a81f1d8d03ccf6c9297acf53536c9459aee42e
Casquinha2 <tmdscasquinha@gmail.com>
2024-10-26 21:45:14 +0100

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,21 +1,19 @@
 from pytubefix import YouTube
-from moviepy.editor import concatenate_videoclips, AudioFileClip, VideoFileClip
+from moviepy.editor import AudioFileClip, VideoFileClip
 import shutil
 
 file = YouTube(input("Introduza o seu video: "))
 
-# little utility to parse out the resolution from a stream description like 1440p
 def get_resolution(s):
     return int(s.resolution[:-1])
 
 
 video = max(
-    filter(lambda s: get_resolution(s) <= 1080,  # filter out sub-1080p streams
-           filter(lambda s: s.type == 'video', file.streams)), # filter out the video streams
-    key=get_resolution  # maximum resolution among those streams
+    filter(lambda s: get_resolution(s) <= 1080,
+           filter(lambda s: s.type == 'video', file.streams)),
+    key=get_resolution
 )
 
-##video = file.streams.get_highest_resolution()
 audio = file.streams.get_audio_only()
 
 video.download('cache', filename='teste.mp4')
@@ -26,6 +24,4 @@ video = VideoFileClip('cache/teste.mp4')
 final_clip = video.set_audio(audio)
 final_clip.write_videofile('videocomsom.mp4', codec='libx264')
 
-
 shutil.rmtree('cache')
-
